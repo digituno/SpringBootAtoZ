f7fab00566a14f72268a3a4d1852304d86edfa8c
digituno <digituno@gmail.com>
2023-02-27 16:12:18 +0900

    feat project init.

---
+++ src/main/java/com/digituno/springbootatoz/SpringBootAtoZApplication.java
@@ -0,0 +1,13 @@
+package com.digituno.springbootatoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAtoZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAtoZApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digituno/springbootatoz/SpringBootAtoZApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digituno.springbootatoz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAtoZApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
